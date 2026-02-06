80b810306d0bb578f43fea462c07b17beb03602d
Artem <muha13artem@gmail.com>
2026-02-06 23:06:21 +0200

    Solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,27 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  // 1. Ми даємо функції ім'я (adder)
+  function adder(value) {
+    if (value === undefined) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+
+    sum += value;
+
+    // 2. ОСЬ ЦЕЙ ПРИЙОМ!
+    // Ми повертаємо посилання на об'єкт цієї ж функції.
+    return adder;
+  }
+
+  // Повертаємо функцію вперше, щоб запустити процес
+  return adder;
 }
 
 module.exports = makeInfinityAdder;
